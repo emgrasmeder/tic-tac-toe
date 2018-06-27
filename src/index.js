f72b8c71cc8b4c11f8a1284b1082c807c7fb93b6
Emily A Grasmeder <egrasmed@thoughtworks.com>
2018-06-27 16:19:23 +0200

    Add prettier and eslint

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import { render } from 'react-dom';
 import './style.css';
 
 class App extends React.Component {
-render() {
-    return (
-      <div>
-        'Hello worlds!'
-      </div>
-    );
+  render() {
+    return <div >Hello world!</div >;
   }
 }
+
 render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
       {
+        enforce: "pre",
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: "eslint-loader",
+        options: { fix: "true" }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }, { loader: "eslint-loader" }],
       },
       {
         test: /\.css$/,
-        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin('dist', {} ),
+    new CleanWebpackPlugin('dist', {}),
     new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css',
     }),
@@ -34,8 +40,8 @@ module.exports = {
       inject: false,
       hash: true,
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
-    new WebpackMd5Hash()
-  ]
+    new WebpackMd5Hash(),
+  ],
 };
